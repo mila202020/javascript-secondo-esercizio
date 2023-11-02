eb1356c9e284aee6537e4dc2c84cd2e0d69de0d6
mila <r.bustamantemullisaca@gmail.com>
2023-11-02 10:02:42 +0100

    javascript esercizio 2

--- javascript esercizio 2.js
+++ javascript esercizio 2.js
@@ -182,6 +182,9 @@ let numerivari = [1,2,3,4,5,6,7,8,9,10,]; // Creazione di un array
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
-numerivari.push(100)
+//numerivari[9]=100;
+//numerivari[numerivari.length-1]= 100;
+numerivari.pop();
+numerivari.push(100);
 /* SCRIVI QUI LA TUA RISPOSTA */
 console.log(numerivari);
